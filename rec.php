d49b762de2aeb9571bd6508e32a49a026d7d92b4
hematt <108283493+hematt@users.noreply.github.com>
2022-07-14 12:33:11 +0530

    Add files via upload

---
+++ rec.php
@@ -0,0 +1,18 @@
+﻿<?php
+// Write a PHP program to
+// draw a rectangle using for loop
+
+$r = 7;
+$c = 9;
+
+for ($i = 1; $i <= $r; $i++) {
+    for ($j = 1; $j <= $c; $j++) {
+        if ($i == 1 || $i == $r || $j == 1 || $j == $c)
+            echo "* ";
+        else
+            echo "  ";
+    }
+    echo "\n";
+}
+
+?>
